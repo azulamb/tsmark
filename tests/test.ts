c3ae9e24a5a0f0824388368065fdbc0a4cb6a320
azulamb <1127785+azulamb@users.noreply.github.com>
2025-07-05 08:09:12 +0900

    Add single test.

--- tests/test.ts
+++ tests/test.ts
@@ -2,7 +2,20 @@ import * as assert from 'jsr:@std/assert';
 import tests from './spec.json' with { type: 'json' };
 import { convertToHTML } from '../src/tsmark.ts';
 
+const testCases = Deno.args.map((arg) => {
+  const id = parseInt(arg, 10);
+  return isNaN(id) ? 0 : id;
+}).filter((id) => {
+  return 0 < id;
+});
+
+let id = 0;
 for (const test of tests) {
+  if (0 < testCases.length) {
+    if (!testCases.includes(++id)) {
+      continue;
+    }
+  }
   const actual = convertToHTML(test.markdown);
   assert.assertEquals(actual, test.html, `Test failed[${test.example}]:`);
 }
